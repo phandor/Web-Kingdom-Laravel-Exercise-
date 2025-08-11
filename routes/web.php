2b78d5fd56643f2263ee048b95c040d8b1fbf22d
lami <dorella.mphan@gmail.com>
2025-08-11 22:53:36 +0200

    Update about Kingdom
    Illidan Stormrage Demon Hunter xD

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about', [
+        'king' => "King Illidan Stormrage xD",
+        'population' => 4348
+    ]);
+});
+
 Route::get('/dungeon', function (){
     return view('dungeon');
 });

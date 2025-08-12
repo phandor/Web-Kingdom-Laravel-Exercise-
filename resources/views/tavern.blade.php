ed7a6f549d199e0fb36e395f4f2d42e5e5b6cbe6
lami <dorella.mphan@gmail.com>
2025-08-12 10:27:59 +0200

    Update tavern.blade.php
    new quests

--- resources/views/tavern.blade.php
+++ resources/views/tavern.blade.php
@@ -1,4 +1,20 @@
-<form action="/fight" method="POST">
+<form action="/dragonfight" method="POST">
 @csrf
+<h1>Dragonfight Quest</h1>
+<p>Reward: 5💎 10XP </p>
+<label for="weapon">Choose your weapon:</label>
+
+<select name="weapon" id="weapon">
+  <option value="sword">Beginner Sword</option>
+  <option value="hammer">Beginner Hammer</option>
+</select>
+
 <button type="submit">Fight the Dragon</button>
+</form>
+
+<form action="/slimemonster" method="POST">
+@csrf
+<h1>Slimemonster Quest</h1>
+<p>Reward: 1💎 🧪mystery Potion 5XP</p>
+<button type="submit">Catch the Slimemonster</button>
 </form>

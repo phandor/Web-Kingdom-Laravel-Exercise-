05818a942f1df058f90039c499c9a81e27c14d05
lami <dorella.mphan@gmail.com>
2025-08-14 12:58:14 +0200

    Add files via upload

---
+++ resources/views/overview.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+
+    </title>
+  </head>
+  <body>
+    <h1>Welcome to {{ $king }}'s Kingdom!</h1>
+
+    <div class="inventury">
+    <h1>Your Inventory contains:</h1>
+    <p></p>
+    </div>
+  </body>
+</html>
